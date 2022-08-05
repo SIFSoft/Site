5cba52962f93fe5c2adcdf66bdc5abb94cdbf30e
Your Nam <Jpsilvabarr@gmail.com>
2022-08-05 12:28:59 -0300

    Modo escuro e responsividade.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ function scrollShandow() {
     document.getElementById("navbar").style.boxShadow = "none"
   }
 }
+
+
+function theme(valor){
+  if(valor == 0){
+    document.getElementById("btnMode").value = 1;
+    document.getElementById("btnMode").innerHTML = "<span title='Modo Claro.' id='modeD' class='material-symbols-outlined'>light_mode</span>";
+    document.getElementById("conteiner").className = "dark"
+  }else if(valor == 1){
+    document.getElementById("btnMode").value = 0;
+    document.getElementById("btnMode").innerHTML = "<span title='Modo Escuro.' id='modeC' class='material-symbols-outlined'>dark_mode</span>";
+    document.getElementById("conteiner").className = "light"
+  }
+}
