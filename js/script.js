78e5206170e1a4420b3ec7c68ab9da8eb778a117
Your Nam <Jpsilvabarr@gmail.com>
2022-08-06 19:22:07 -0300

    slide

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
+let count = 1, controle = false
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+  slideAuto()
+}, 5000);
+
+function slideAuto(){
+  if(controle){
+    count = 1
+    controle = false
+  }else
+    count++;
+  if(count > 3){
+    count = 2;
+    controle = true;
+  }
+  document.getElementById("radio"+count).checked = true;
+}
+
 window.onscroll = function() {scrollShandow()};
- 
+
+
+
+
 function scrollShandow() {
-  document.getElementById("navbar").style.transition = "0.6s ease-in-out";
+  document.getElementById("navbar").style.transition = "0.5s ease-in-out";
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.boxShadow = "0px 3px 10px rgba(0, 0, 0, 0.15)" 
   } else{
@@ -21,3 +44,4 @@ function theme(valor){
     document.getElementById("conteiner").className = "light"
   }
 }
+
