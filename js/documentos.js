45c22a669776473a1c51077ef2ea50638449f5f4
GustavoDias496 <88061007+GustavoDias496@users.noreply.github.com>
2022-08-19 09:52:34 -0300

    Pag documentos e contato
    Páginas contato e documentos sem estilo e correção de links na nav

--- js/documentos.js
+++ js/documentos.js
@@ -1,13 +1,36 @@
 const url = 'https://sifsoft-api.herokuapp.com/documents/?format=json'
 
+const carregando = document.querySelector('.carregando');
+const documentsContainer = document.querySelector('.document-container')
+
 
 async function getAllDocuments(){
     const response = await fetch(url)
 
-
     const data = await response.json();
 
-    console.log(data);
+    data.map((documento) =>{
+        
+        const div = document.createElement("div")
+        const title = document.createElement("h2")
+        const datalancamento = document.createElement("p")
+        const tipo = document.createElement("p")
+        const link = document.createElement("a")
+
+
+        title.innerText = documento.title
+        datalancamento.innerText = documento.date
+        tipo.innerText = documento.type
+        link.innerText = "Visualizar"
+        link.setAttribute("href", `${documento.link}`)
+
+        div.appendChild(title)
+        div.appendChild(datalancamento)
+        div.appendChild(tipo)
+        div.appendChild(link)
+
+        documentsContainer.appendChild(div)
+    });
 }
 
 getAllDocuments();
